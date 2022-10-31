e2c4aa9379d3aa53371c5c5a70d017379fadc11a
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2022-10-30 22:52:46 -0300

    Criado rota para criar user adm

--- app/controllers/users/routes.py
+++ app/controllers/users/routes.py
@@ -1,5 +1,9 @@
+from crypt import methods
 from flask import render_template, flash, redirect, url_for, Blueprint, request, abort
+from flask_login import current_user, login_user
 from app.controllers.users.formUser import LoginForm
+from app import db, bcrypt
+from app.models.bdEternusLanches import (Users, Enderecos, Acessos)
 
 users = Blueprint('users', __name__)
 
@@ -7,6 +11,56 @@ users = Blueprint('users', __name__)
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Logado com sucesso!','success')
-        return redirect(url_for('home.index'))
+        try:
+            user = Users.query.filter_by(login=form.login.data.lower()).first_or_404()
+            if user and bcrypt.check_password_hash(user.password, form.password.data.lower()):
+                login_user(user=user, remember=form.remember.data)
+                next_page = request.args.get('next')
+                flash('Logado com sucesso!','success')
+                return redirect(next_page) if next_page else redirect(url_for('home.index'))
+            else:
+                flash('Error, verifique Login/Senha!', 'danger')
+        except Exception as e:
+            print(f'Error: {e}')
+
     return render_template('users/login.html', title='Login', legenda='Login', form=form)
+
+@users.route('/createFuncionario/<login>/<password>', methods=['GET'])
+def createFuncionario(login, password):
+    if current_user.is_authenticated:
+        return redirect(url_for('home.index'))
+
+    user = Users.query.all()
+
+    if not user:
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        try:
+            endereco = Enderecos(rua='Rua 1', numero=123, cidade='Guaratingueta', bairro='Nova Guara', cep='13230-20', principal=True)
+            db.session.add(endereco)
+            db.session.commit()
+        except Exception as e:
+            db.session.flush()
+            db.session.rollback()
+            print(f'Errors: {e}')
+        
+        try:
+            acesso = Acessos(tipo='Funcionario')
+            db.session.add(acesso)
+            db.session.commit()
+        except Exception as e:
+            db.session.flush()
+            db.session.rollback()
+            print(f"Errors: {e}")
+        
+        try:
+            user = Users(nome=login.lower(), email=login.lower()+'@eternus.com.br', password=hashed_password, endereco=[endereco], acesso=[acesso])
+            db.session.add(user)
+            db.session.commit()
+        except Exception as e:
+            db.session.flush()
+            db.session.rollback()
+            print(f'Error: {e}')
+            flash('Erro ao criar usuário!', 'danger')
+        
+        flash('Users criado com sucesso!', 'success')
+        return redirect(url_for('home.index'))
---
+++ app/models/bdEternusLanches.py
@@ -0,0 +1,174 @@
+from datetime import datetime
+from app import db, login_manager
+from flask_login import UserMixin
+
+
+entregaUsers = db.Table(
+    'entregaUsers',
+    db.Column('idEntrega', db.Integer, db.ForeignKey('Entregas.id')),
+    db.Column('idUsers', db.Integer, db.ForeignKey('Users.id'))
+)
+
+pedidoUsers = db.Table(
+    'pedidoUsers',
+    db.Column('idPedido', db.Integer, db.ForeignKey('Pedidos.id')),
+    db.Column('idUsers', db.Integer, db.ForeignKey('Users.id'))
+)
+
+enderecoUsers = db.Table(
+    'enderecoUsers',
+    db.Column('idUsers', db.Integer, db.ForeignKey('Users.id')),
+    db.Column('idEndereco', db.Integer, db.ForeignKey('Enderecos.id'))
+)
+
+acessoUsers = db.Table(
+    'acessoUsers',
+    db.Column('idUsers', db.Integer, db.ForeignKey('Users.id')),
+    db.Column('idAcesso', db.Integer, db.ForeignKey('Acessos.id'))
+)
+
+pedidoLanches = db.Table(
+    'pedidoLanches',
+    db.Column('idLanche', db.Integer, db.ForeignKey('Lanches.id')),
+    db.Column('idPedido', db.Integer, db.ForeignKey('Pedidos.id'))
+)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+class Lanches(db.Model):
+    __tablename__ = 'Lanches'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(40), unique=True, nullable=True)
+    valor = db.Column(db.Float, nullable=False)
+    ingrediente = db.Column(db.String(150), nullable=False)
+
+    def __init__(self, nome='', valor=0.00, ingrediente='....') -> None:
+        self.nome = nome
+        self.valor = valor
+        self.ingrediente = ingrediente
+
+    def __repr__(self) -> str:
+        return f"Lanches('{self.nome}', '{self.valor}', '{self.ingrediente}')"
+
+
+class Mesas(db.Model):
+    __tablename__ = 'Mesas'
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.Integer, unique=True, nullable=False)
+    livre = db.Column(db.Boolean, nullable=False, default=True)
+
+    pedidos = db.relationship('Pedidos', backref='mesas', lazy=True)
+
+    def __init__(self, numero=0, livre=True) -> None:
+        self.numero = numero
+        self.livre = livre
+
+    def __repr__(self) -> str:
+        return f"Mesas('{self.numero}', '{self.livre}')"
+
+
+class Pedidos(db.Model):
+    __tablename__ = 'Pedidos'
+    id = db.Column(db.Integer, primary_key=True)
+    dataHora = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    codPedido = db.Column(db.String(15), nullable=False, unique=True)
+    idMesa = db.Column(db.Integer, db.ForeignKey('Mesas.id'), nullable=False)
+
+    entrega = db.relationship('Entregas', backref='pedidos', lazy=True)
+    user = db.relationship('Users', secondary=pedidoUsers, backref='pedidos')
+    lanche = db.relationship('Lanches', secondary=pedidoLanches, backref='pedidos')
+
+    def __init__(self, dataHora=datetime.utcnow, codPedido='ET0123456789', idMesa=0) -> None:
+        self.dataHora = dataHora
+        self.codPedido = codPedido
+        self.idMesa = idMesa
+
+    def __repr__(self) -> str:
+        return f"Pedidos('{self.dataHora}', '{self.codPedido}')"
+
+
+class Entregas(db.Model):
+    __tablename__ = 'Entregas'
+    id = db.Column(db.Integer, primary_key=True)
+    taxaEntrega = db.Column(db.Float)
+    idPedido = db.Column(db.Integer, db.ForeignKey('Pedidos.id'), nullable=False)
+    user = db.relationship('Users', secondary=entregaUsers, backref='entregas')
+
+    def __init__(self, taxaEntrega=0.00, idPedido=0, user=[]) -> None:
+        self.taxaEntrega = taxaEntrega
+        self.idPedido = idPedido
+        self.user = user
+
+    def __repr__(self) -> str:
+        return f"Entregas('{self.taxaEntrega}')"
+
+
+class Users(db.Model, UserMixin):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(40), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
+    password = db.Column(db.String(60), unique=True, nullable=False)
+
+    endereco = db.relationship('Enderecos', secondary=enderecoUsers, backref='users')
+    acesso = db.relationship('Acessos', secondary=acessoUsers, backref='users')
+
+    def __init__(self, nome='', email='', password='', endereco=[], acesso=[]) -> None:
+        self.nome = nome
+        self.email = email
+        self.password = password
+        self.endereco = endereco
+        self.acesso = acesso
+
+    def __repr__(self) -> str:
+        return f"Users('{self.nome}', '{self.email}')"
+
+
+class Telefones(db.Model):
+    __tablename__ = 'Telefones'
+    id = db.Column(db.Integer, primary_key=True)
+    numero = db.Column(db.Integer, nullable=False, unique=True)
+
+    def __init__(self, numero=0) -> None:
+        self.numero = numero
+
+    def __repr__(self) -> str:
+        return f"Telefone('{self.numero}')"
+
+
+class Acessos(db.Model):
+    __tablename__ = 'Acessos'
+    id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(15), nullable=False, unique=True)
+
+    def __init__(self, tipo='') -> None:
+        self.tipo = tipo
+
+    def __repr__(self) -> str:
+        return f"Acessos('{self.tipo}')"
+
+
+class Enderecos(db.Model):
+    __tablename__ = 'Enderecos'
+    id = db.Column(db.Integer, primary_key=True)
+    rua = db.Column(db.String(40), nullable=False)
+    numero = db.Column(db.Integer, nullable=False)
+    cidade = db.Column(db.String(40), nullable=False)
+    bairro = db.Column(db.String(40), nullable=False)
+    cep = db.Column(db.String(14), nullable=False)
+    principal = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, rua='', numero=0, cidade='', bairro='', cep='', principal=True) -> None:
+        self.rua = rua
+        self.numero = numero
+        self.cidade = cidade
+        self.bairro = bairro
+        self.cep = cep
+        self.principal = principal
+
+    def __repr__(self) -> str:
+        return f"Endereco('{self.rua}', '{self.numero}', '{self.cidade}', '{self.bairro}', '{self.cep}', '{self.principal}')"
