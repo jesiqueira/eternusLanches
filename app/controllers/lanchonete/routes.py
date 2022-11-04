4ca11899f2453559d2b4f6b5c5140ad450a0c2cc
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2022-11-04 11:51:52 -0300

    Efetuado correções para acessar páginas da lanchone para área de funcionários

--- app/controllers/lanchonete/routes.py
+++ app/controllers/lanchonete/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, Blueprint, request, abort
-from flask_login import current_user
+from flask_login import current_user, login_required
 from app.controllers.lanchonete.lanchoneteForm import MesasForm
 
 lanchonete = Blueprint('lanchonete', __name__)
@@ -8,28 +8,87 @@ lanchonete = Blueprint('lanchonete', __name__)
 @lanchonete.route('/salaoLanchonete', methods=['GET'])
 def salaoLanchonete():
     if current_user.is_authenticated:
-        print(current_user.acesso[0].tipo)
-    return render_template('lanchonete/salao.html')
+        if current_user.acesso[0].tipo == 'Funcionario':
+            form = MesasForm()
+            return render_template('lanchonete/salao.html')
+        else:
+            flash('Não tem permissão para acessar essa página', 'danger')
+            return redirect(url_for('home.index'))
+    else:
+        flash('Faça o login para acessar essa página.', 'danger')
+        return redirect(url_for('users.login'))
 
 
 @lanchonete.route('/mesas', methods=['GET'])
+@login_required
 def mesas():
-    form = MesasForm()
-    return render_template('lanchonete/mesas.html', title='Mesas', form=form)
+    if current_user.is_authenticated:
+        if current_user.acesso[0].tipo == 'Funcionario':
+            form = MesasForm()
+            return render_template('lanchonete/mesas.html', title='Mesas', form=form)
+        else:
+            flash('Não tem permissão para acessar essa página', 'danger')
+            return redirect(url_for('home.index'))
+    else:
+        flash('Faça o login para acessar essa página.', 'danger')
+        return redirect(url_for('users.login'))
 
 
 @lanchonete.route('/lanches', methods=['GET'])
+@login_required
 def lanches():
-    return render_template('lanchonete/lanches.html', title='Lanches')
-    
+    if current_user.is_authenticated:
+        if current_user.acesso[0].tipo == 'Funcionario':
+            form = MesasForm()
+            return render_template('lanchonete/lanches.html', title='Lanches')
+        else:
+            flash('Não tem permissão para acessar essa página', 'danger')
+            return redirect(url_for('home.index'))
+    else:
+        flash('Faça o login para acessar essa página.', 'danger')
+        return redirect(url_for('users.login'))
+
+
 @lanchonete.route('/porcoes', methods=['GET'])
+@login_required
 def porcoes():
-    return render_template('lanchonete/porcoes.html', title='Porcoes')
+    if current_user.is_authenticated:
+        if current_user.acesso[0].tipo == 'Funcionario':
+            form = MesasForm()
+            return render_template('lanchonete/porcoes.html', title='Porcoes')
+        else:
+            flash('Não tem permissão para acessar essa página', 'danger')
+            return redirect(url_for('home.index'))
+    else:
+        flash('Faça o login para acessar essa página.', 'danger')
+        return redirect(url_for('users.login'))
+
 
 @lanchonete.route('/bebidas', methods=['GET'])
+@login_required
 def bebidas():
-    return render_template('lanchonete/bebidas.html', title='Bebidas')
+    if current_user.is_authenticated:
+        if current_user.acesso[0].tipo == 'Funcionario':
+            form = MesasForm()
+            return render_template('lanchonete/bebidas.html', title='Bebidas')
+        else:
+            flash('Não tem permissão para acessar essa página', 'danger')
+            return redirect(url_for('home.index'))
+    else:
+        flash('Faça o login para acessar essa página.', 'danger')
+        return redirect(url_for('users.login'))
+
 
 @lanchonete.route('/pedidos', methods=['GET'])
+@login_required
 def pedidos():
-    return render_template('lanchonete/pedidos.html', title='Pedidos')
+    if current_user.is_authenticated:
+        if current_user.acesso[0].tipo == 'Funcionario':
+            form = MesasForm()
+            return render_template('lanchonete/pedidos.html', title='Pedidos')
+        else:
+            flash('Não tem permissão para acessar essa página', 'danger')
+            return redirect(url_for('home.index'))
+    else:
+        flash('Faça o login para acessar essa página.', 'danger')
+        return redirect(url_for('users.login'))
