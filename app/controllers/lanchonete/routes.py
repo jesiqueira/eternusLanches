ec43c335e92ad81f0e2c88896622cd025e047251
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2022-11-04 21:55:15 -0300

    Finalizado rota para remover mesa

--- app/controllers/lanchonete/routes.py
+++ app/controllers/lanchonete/routes.py
@@ -3,6 +3,7 @@ from flask_login import current_user, login_required
 from app.controllers.lanchonete.lanchoneteForm import MesasForm, MesaNovaForm
 from app.models.bdEternusLanches import Mesas
 from app import db
+from sqlalchemy import exc
 
 lanchonete = Blueprint('lanchonete', __name__)
 
@@ -86,14 +87,49 @@ def atualizarMesa():
                 mesa.numero = int(form.numero.data)
                 try:
                     db.session.commit()
-                except Exception as e:
-                    print(f'Error: {e}')
+                except exc.IntegrityError as e:
+                    # print(f'Error: {e}')
                     db.session.flush()
                     db.session.rollback()
+                    flash('Erro ao atualizar já existe mesa com número informado.', 'danger')
+                    return redirect(url_for('lanchonete.mesas'))
                 flash('Mesa atualizada com sucesso.', 'success')
                 return redirect(url_for('lanchonete.mesas'))
             else:
                 return render_template('lanchonete/updateMesa.html', title='Atualizar Mesa', form=form)
+        else:
+            flash('Não tem permissão para acessar essa página', 'danger')
+            return redirect(url_for('home.index'))
+    else:
+        flash('Faça o login para acessar essa página.', 'danger')
+        return redirect(url_for('users.login'))
+
+
+@lanchonete.route('/removerMesa', methods=['POST', 'GET'])
+@login_required
+def removerMesa():
+    if current_user.is_authenticated:
+        if current_user.acesso[0].tipo == 'Funcionario':
+            form = MesaNovaForm()
+            if request.method == 'POST' and request.form.get('idMesa'):
+                mesa = Mesas.query.get(int(request.form.get('idMesa')))
+                form.id_mesa.data = mesa.id
+                form.numero.data = mesa.numero
+                return render_template('lanchonete/removerMesa.html', title='Remover Mesa', form=form)
+            if form.validate_on_submit():
+                mesa = Mesas.query.get(int(form.id_mesa.data))
+                mesa.numero = int(form.numero.data)
+                try:
+                    db.session.delete(mesa)
+                    db.session.commit()
+                except Exception as e:
+                    print(f'Error: {e}')
+                    db.session.flush()
+                    db.session.rollback()
+                flash('Mesa removida com sucesso.', 'success')
+                return redirect(url_for('lanchonete.mesas'))
+            else:
+                return render_template('lanchonete/removerMesa.html', title='Remover Mesa', form=form)
         else:
             flash('Não tem permissão para acessar essa página', 'danger')
             return redirect(url_for('home.index'))
