9032d596ddcddaba9fbd200efe1cae3ad74fc032
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2022-11-12 21:16:51 -0300

    Modificado users cadastro e lanches.js

--- app/static/js/lanche.js
+++ app/static/js/lanche.js
@@ -1,20 +1,20 @@
 function editarLanche(idLanche) {
-  document.querySelector('#idLanche').value = idLanche
+    document.querySelector("#idLanche").value = idLanche
 }
 
 function updateLanche() {
-  let idLanche = document.querySelector("#idLanche").value;
-  let url = '/atualizarlanche'
+    let idLanche = document.querySelector("#idLanche").value
+    let url = "/atualizarlanche"
 
-  let form = document.createElement('form')
-  form.action = url
-  form.method = 'POST'
+    let form = document.createElement("form")
+    form.action = url
+    form.method = "POST"
 
-  let input = document.createElement('input')
-  input.type = 'hidden'
-  input.name = 'idLanche'
-  input.value = idLanche
-  form.appendChild(input)
-  document.body.appendChild(form)
-  form.submit()
+    let input = document.createElement("input")
+    input.type = "hidden"
+    input.name = "idLanche"
+    input.value = idLanche
+    form.appendChild(input)
+    document.body.appendChild(form)
+    form.submit()
 }
