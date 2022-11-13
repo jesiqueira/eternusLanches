a7c5b2300677c99e54530fc2a10f3f59db13d151
Jose Edmar de Siqueira <edmar.ade@gmail.com>
2022-11-12 22:28:46 -0300

    Criado e finalizado rota para excluir lanche/página removerlanche.html

--- app/static/js/lanche.js
+++ app/static/js/lanche.js
@@ -18,3 +18,19 @@ function updateLanche() {
     document.body.appendChild(form)
     form.submit()
 }
+
+function deleteLanche() {
+    let idLanche = document.querySelector("#idLanche").value
+    let url = "/removerlanche"
+    let form = document.createElement("form")
+    form.action = url
+    form.method = "POST"
+
+    let input = document.createElement("input")
+    input.type = "hidden"
+    input.name = "idLanche"
+    input.value = idLanche
+    form.appendChild(input)
+    document.body.appendChild(form)
+    form.submit()
+}
